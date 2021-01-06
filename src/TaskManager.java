9d59c5e8e76c71e524edd43e14307088765c22d8
vatpat <66856744+vatpat@users.noreply.github.com>
2021-01-05 23:12:52 -0600

    Started Project
    Main Window is displayed. Next Steps (in no particular order) include: add GUI elements and event listeners, add Task class and incorporate with event listeners, and add File I/O to save and load the task data

---
+++ src/TaskManager.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TaskManager extends Frame {
+  public TaskManager() {
+    setTitle("Task Planner");
+    setSize(700, 700);
+    
+    // Handling window closing when exited
+    addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        dispose();
+      }
+    });
+    
+    setLayout(null); // no layout
+    setVisible(true);
+  }
+  
+  public static void main(String[] args) {
+    TaskManager t = new TaskManager();
+  }
+}
